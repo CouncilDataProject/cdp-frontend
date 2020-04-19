66d2d93f3243e82dfaacd231b65e927de53f5b46
To Huynh <huynh.nto@gmail.com>
2020-04-18 20:01:06 -0700

    Remove withKnobs from indivdial story and add it globally

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,11 +3,13 @@ import React from "react";
 import { addDecorator } from "@storybook/react";
 import { withA11y } from "@storybook/addon-a11y";
 import { withInfo } from "@storybook/addon-info";
+import { withKnobs } from "@storybook/addon-knobs";
 import "semantic-ui-css/semantic.min.css";
 import { Segment } from "semantic-ui-react";
 
 addDecorator(withInfo);
 addDecorator(withA11y);
+addDecorator(withKnobs);
 
 // Wrap a simple container around all stories.
 addDecorator((storyFn) => (
