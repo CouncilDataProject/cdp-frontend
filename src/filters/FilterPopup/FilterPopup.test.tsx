b0d9d0dae26c8d00c98e592aa51b348dd4a7ed80
To Huynh <huynh.nto@gmail.com>
2020-04-19 13:54:23 -0700

    Add test case to cover opening FilterPopup

--- src/filters/FilterPopup/FilterPopup.test.tsx
+++ src/filters/FilterPopup/FilterPopup.test.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 
-import { shallow, ShallowWrapper } from "enzyme";
+import { mount, ReactWrapper, shallow, ShallowWrapper } from "enzyme";
 
 import FilterPopup, { FilterPopupProps } from "./FilterPopup";
 
 describe("FilterPopup", () => {
-  let filterPopup: ShallowWrapper<FilterPopupProps>;
   const clearMock = jest.fn();
   const getTextRepMock = jest.fn();
   const isActiveMock = jest.fn();
@@ -14,8 +13,12 @@ describe("FilterPopup", () => {
   const setPopupIsOpenMock = jest.fn();
   const handlePopupCloseMock = jest.fn();
 
-  beforeEach(() => {
-    filterPopup = shallow(
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  test("Calls setPopupisOpen callback to open popup", () => {
+    const filterPopupMount: ReactWrapper<FilterPopupProps> = mount(
       <FilterPopup
         clear={clearMock}
         getTextRep={getTextRepMock}
@@ -28,23 +31,42 @@ describe("FilterPopup", () => {
         Test
       </FilterPopup>
     );
-  });
-
-  afterEach(() => {
-    jest.resetAllMocks();
-  });
-
-  test("Calls clear filter callback", () => {
-    expect(clearMock).toHaveBeenCalledTimes(0);
-    filterPopup.find("Button[size='mini']").at(0).simulate("click");
-    expect(clearMock).toHaveBeenCalledTimes(1);
-  });
-
-  test("Calls handPopupClose callback", () => {
     expect(setPopupIsOpenMock).toHaveBeenCalledTimes(0);
-    expect(handlePopupCloseMock).toHaveBeenCalledTimes(0);
-    filterPopup.find("Button[size='mini']").at(1).simulate("click");
+    filterPopupMount.find("Button").simulate("click");
     expect(setPopupIsOpenMock).toHaveBeenCalledTimes(1);
-    expect(handlePopupCloseMock).toHaveBeenCalledTimes(1);
+  });
+
+  describe("FilterPopup shallow render", () => {
+    let filterPopup: ShallowWrapper<FilterPopupProps>;
+
+    beforeEach(() => {
+      filterPopup = shallow(
+        <FilterPopup
+          clear={clearMock}
+          getTextRep={getTextRepMock}
+          isActive={isActiveMock}
+          header={header}
+          popupIsOpen={popupIsOpen}
+          setPopupIsOpen={setPopupIsOpenMock}
+          handlePopupClose={handlePopupCloseMock}
+        >
+          Test
+        </FilterPopup>
+      );
+    });
+
+    test("Calls clear filter callback", () => {
+      expect(clearMock).toHaveBeenCalledTimes(0);
+      filterPopup.find("Button[size='mini']").at(0).simulate("click");
+      expect(clearMock).toHaveBeenCalledTimes(1);
+    });
+
+    test("Calls handPopupClose callback", () => {
+      expect(setPopupIsOpenMock).toHaveBeenCalledTimes(0);
+      expect(handlePopupCloseMock).toHaveBeenCalledTimes(0);
+      filterPopup.find("Button[size='mini']").at(1).simulate("click");
+      expect(setPopupIsOpenMock).toHaveBeenCalledTimes(1);
+      expect(handlePopupCloseMock).toHaveBeenCalledTimes(1);
+    });
   });
 });
