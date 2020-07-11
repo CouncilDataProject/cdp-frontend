2e93db0af149480896cbe4c48e5bc764d1b263f5
To Huynh <huynh.nto@gmail.com>
2020-07-11 11:57:57 -0700

    Add Oldest and Newest first options to SelectSorting

--- src/filters/SelectSorting/SelectSorting.tsx
+++ src/filters/SelectSorting/SelectSorting.tsx
@@ -3,42 +3,46 @@ import React, { FormEvent, FunctionComponent } from "react";
 import { Checkbox, CheckboxProps, Form } from "semantic-ui-react";
 
 import { FilterState } from "../reducer";
-import { sortBy, SortByOption } from "./getSortingText";
+import { SortOption } from "./getSortingText";
 
 export interface SelectSortingProps {
-  /**The list of sort by options.
-   * `date` means sort by date in descending order. `value` means sort by search results' relevancy in descending order.
-   * */
-  sortByOptions: SortByOption[];
+  /**The list of sort options. */
+  sortOptions: SortOption[];
   /**The sorting state. */
   state: FilterState<string>;
   /**Callback to update the sorting state. */
   update(keyName: string, dataValue: string): void;
+  /**Callback to close popup.*/
+  onPopupClose(): void;
 }
 
 /**A list of radio checkboxes to allow for selecting sorting options. */
 const SelectSorting: FunctionComponent<SelectSortingProps> = ({
-  sortByOptions,
+  sortOptions,
   state,
   update,
+  onPopupClose,
 }: SelectSortingProps) => {
   const onChange = (e: FormEvent<HTMLInputElement>, data: CheckboxProps) => {
-    update(data.name as string, data.value as string);
+    const [by, order] = (data.value as string).split("#");
+    update("by", by);
+    update("order", order);
+    update("label", data.label as string);
+    onPopupClose();
   };
 
   return (
     <Form>
-      {sortByOptions.map((byOption) => (
+      {sortOptions.map((sortOption) => (
         <Form.Field
-          checked={state.by === byOption}
+          checked={state.label === sortOption.label}
           control={Checkbox}
-          id={`form-checkbox-control-${byOption}`}
-          key={byOption}
-          label={sortBy[byOption as SortByOption]}
-          name="by"
+          id={`form-checkbox-control-${sortOption.label}`}
+          key={sortOption.label}
+          label={sortOption.label}
           onChange={onChange}
           radio
-          value={byOption}
+          value={`${sortOption.by}#${sortOption.order}`}
         />
       ))}
     </Form>
