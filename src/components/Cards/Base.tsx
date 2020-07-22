766db4772f72449ebf754dfc228845521598ecb1
Trung Ngo <trungvngo@gmail.com>
2020-07-22 09:21:41 -0700

    set content and image container size; update borders

--- src/components/Cards/Base.tsx
+++ src/components/Cards/Base.tsx
@@ -2,10 +2,11 @@ import React, { ReactNode } from "react";
 import styled from "@emotion/styled";
 import colors from "../../styles/colors";
 
-const CARD_MAX_WIDTH = "554px";
-const CARD_BORDER_RADIUS = "0.3rem";
+const CARD_MAX_WIDTH = "544px";
+const CARD_BORDER_RADIUS = "5px";
 const CARD_BORDER_COLOR = colors.lightgrey;
 const SMALL_SCREEN_BREAKPOINT = "768px";
+const IMAGE_CONTAINER_MIN_HEIGHT = "200px";
 
 const CardContainer = styled.div`
   display: flex;
@@ -23,7 +24,10 @@ type CardImageContainerProps = {
 };
 
 const CardImageContainer = styled.div`
+  /* position relative so that the overlay text can be absolutely positioned */
   position: relative;
+  /* set min-height for image to prevent div collapsing in small screens */
+  min-height: ${IMAGE_CONTAINER_MIN_HEIGHT};
   background-image: linear-gradient(
       transparent 50%,
       rgba(51, 51, 51, 0.1) 60%,
@@ -32,16 +36,16 @@ const CardImageContainer = styled.div`
       rgba(51, 51, 51, 0.8) 100%
     ),
     url(${(props: CardImageContainerProps) => props.imgSrc});
-  width: 100%;
-  height: 100%;
-  /* TODO: adjust min dimensions */
-  min-height: 200px;
-  max-width: 220px;
   object-fit: cover;
+
+  /* desktop styles */
+  @media (min-width: ${SMALL_SCREEN_BREAKPOINT}) {
+    width: 40%;
+  }
 `;
 
 const CardContentContainer = styled.div`
-  border: 2px solid ${CARD_BORDER_COLOR};
+  border: 1px solid ${CARD_BORDER_COLOR};
   border-top: none;
   border-radius: 0 0 ${CARD_BORDER_RADIUS} ${CARD_BORDER_RADIUS};
   padding: 1rem;
@@ -49,8 +53,9 @@ const CardContentContainer = styled.div`
   /* desktop styles */
   @media (min-width: ${SMALL_SCREEN_BREAKPOINT}) {
     border-left: none;
-    border-top: 2px solid ${CARD_BORDER_COLOR};
+    border-top: 1px solid ${CARD_BORDER_COLOR};
     border-radius: 0 ${CARD_BORDER_RADIUS} ${CARD_BORDER_RADIUS} 0;
+    width: 60%;
   }
 `;
 
@@ -59,6 +64,7 @@ type CardProps = {
   imageOverlayContent: ReactNode;
   content: ReactNode;
 };
+
 export const Card = (props: CardProps) => {
   return (
     <CardContainer>
