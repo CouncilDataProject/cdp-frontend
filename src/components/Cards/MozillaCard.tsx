15e3eb63d99ef55ae863ecf9213e1fd6b036c437
Hawk Ticehurst <hawkticehurst@gmail.com>
2020-11-25 23:26:29 -0800

    Initial creation of MozillaCard component

---
+++ src/components/Cards/MozillaCard.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import styled from "@emotion/styled";
+import "@mozilla-protocol/core/protocol/css/protocol.css";
+
+const DateOverlay = styled.p`
+  position: absolute;
+  z-index: 1;
+  color: white;
+  font-size: 30px;
+  font-weight: bolder;
+  top: 75%;
+  left: 3%;
+`;
+
+type MeetingCardProps = {
+  cardLink: string;
+  imgSrc: string;
+  imgAlt: string;
+  imgOverlayText: string;
+  committee: string;
+  tags: [string];
+  excerpt?: string;
+};
+
+export const MozillaCard = ({
+  cardLink,
+  imgSrc,
+  imgAlt,
+  imgOverlayText,
+  committee,
+  tags,
+  excerpt,
+}: MeetingCardProps) => {
+  const tagString = tags.join(` • `);
+
+  return (
+    <section className="mzp-c-card mzp-has-aspect-16-9">
+      <a className="mzp-c-card-block-link" href={cardLink}>
+        <div className="mzp-c-card-media-wrapper">
+          <DateOverlay>{imgOverlayText}</DateOverlay>
+          <img className="mzp-c-card-image" src={imgSrc} alt={imgAlt} />
+        </div>
+        <div className="mzp-c-card-content">
+          <div className="mzp-c-card-tag">Committee</div>
+          <h2 className="mzp-c-card-title">{committee}</h2>
+          {excerpt ? <p className="mzp-c-card-desc">{`"${excerpt}"`}</p> : null}
+          <p className="mzp-c-card-meta">Tags: {tagString}</p>
+        </div>
+      </a>
+    </section>
+  );
+};
