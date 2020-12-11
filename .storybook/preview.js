61e52e4f7948734565753343a32fdadeb82a060d
Hawk Ticehurst <hawkticehurst@gmail.com>
2020-12-10 17:14:40 -0800

    Add config for story sorting, doc theme, and canvas background colors. Also replace old container decorator with a margin decorator

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,17 +1,46 @@
 import React from "react";
-
 import { addDecorator } from "@storybook/react";
 import { withA11y } from "@storybook/addon-a11y";
 import { withInfo } from "@storybook/addon-info";
 import { withKnobs } from "@storybook/addon-knobs";
-import "semantic-ui-css/semantic.min.css";
-import { Segment } from "semantic-ui-react";
+import { themes } from "@storybook/theming";
 
 addDecorator(withInfo);
 addDecorator(withA11y);
 addDecorator(withKnobs);
 
-// Wrap a simple container around all stories.
-addDecorator((storyFn) => (
-  <Segment>{storyFn()}</Segment>
-));
+export const decorators = [
+  (Story) => (
+    <div style={{ margin: "1rem" }}>
+      <Story />
+    </div>
+  ),
+];
+
+export const parameters = {
+  options: {
+    storySort: {
+      order: ["Getting Started", "Library"],
+    },
+  },
+  docs: {
+    theme: themes.dark,
+  },
+  backgrounds: {
+    default: "light",
+    values: [
+      {
+        name: "light",
+        value: "#f9f9f9",
+      },
+      {
+        name: "grey",
+        value: "#929396",
+      },
+      {
+        name: "dark",
+        value: "#333",
+      },
+    ],
+  },
+};
