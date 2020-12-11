d3dd7c9381cd695a1f801bb8bf5ab3c6fcd82493
Hawk Ticehurst <hawkticehurst@gmail.com>
2020-12-11 11:59:57 -0800

    Fix invalid storybook glob pattern

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  stories: ["../src/**/*.stories.(tsx|mdx)"],
+  stories: ["../src/**/*.stories.@(tsx|mdx)"],
   addons: [
     "@storybook/addon-a11y",
     "@storybook/addon-actions",
