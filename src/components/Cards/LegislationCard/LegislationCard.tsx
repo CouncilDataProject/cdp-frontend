8257730ad83408b06a1376296b014726f12828e3
To Huynh <huynh.nto@gmail.com>
2021-02-21 21:34:28 -0800

    Add legislation card

--- src/components/Cards/LegislationCard/LegislationCard.tsx
+++ src/components/Cards/LegislationCard/LegislationCard.tsx
@@ -1,9 +1,18 @@
 import React, { FC } from "react";
 import styled from "@emotion/styled";
-import colors from "../../../styles/colors";
-import { fontSizes } from "../../../styles/fonts";
+
+import LegislationAdoptedIcon from "./LegislationAdoptedIcon";
+import LegislationRejectedIcon from "./LegislationRejectedIcon";
+import LegislationInProgressIcon from "./LegislationInProgressIcon";
 
 import "@mozilla-protocol/core/protocol/css/protocol.css";
+
+const StatusIconContainer = styled.div({
+  float: "right",
+  marginRight: 8,
+  height: 24,
+  width: 24,
+});
 
 export interface LegislationCardProps {
   /**The name of the legislation */
@@ -11,27 +20,36 @@ export interface LegislationCardProps {
   /**The status of legislation. If legislation is in progress then the status is Next Meeting */
   status: string;
   /**The legislation status's date or the date of the next meeting */
-  date: string;
+  date?: string;
   /** List of legislation keywords */
   tags: string[];
-  /**Internal link to the legislation's details */
-  link: string;
 }
 
 const LegislationCard: FC<LegislationCardProps> = ({ name, status, date, tags, link }) => {
   const tagString = tags.join(` • `);
 
+  let statusIcon;
+
+  if (status === "Adopted") {
+    statusIcon = <LegislationAdoptedIcon />;
+  } else if (status === "Rejected") {
+    statusIcon = <LegislationRejectedIcon />;
+  } else {
+    statusIcon = <LegislationInProgressIcon />;
+  }
+
   return (
     <section className="mzp-c-card mzp-has-aspect-16-9">
-      <a className="mzp-c-card-block-link" href={link}>
+      <div className="mzp-c-card-block-link">
         <div className="mzp-c-card-content">
           <h2 className="mzp-c-card-title">{name}</h2>
           <p className="mzp-c-card-meta">{status}</p>
+          <StatusIconContainer>{statusIcon}</StatusIconContainer>
           <p className="mzp-c-card-desc">{date}</p>
           <p className="mzp-c-card-meta">Tags</p>
           <p className="mzp-c-card-desc">{tagString}</p>
         </div>
-      </a>
+      </div>
     </section>
   );
 };
---
+++ src/components/Cards/LegislationCard/index.ts
@@ -0,0 +1,1 @@
+export { default as LegislationCard } from "./LegislationCard";
