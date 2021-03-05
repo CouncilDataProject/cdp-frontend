690dd60b121cb01ea210081d6e2a9adb820a3059
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-03-04 21:13:01 -0800

    Remove date overlay and cardLink prop from meeting card and make the meeting date the card title

--- src/components/Cards/MeetingCard/MeetingCard.tsx
+++ src/components/Cards/MeetingCard/MeetingCard.tsx
@@ -1,32 +1,19 @@
 import React from "react";
-import styled from "@emotion/styled";
 import "@mozilla-protocol/core/protocol/css/protocol.css";
 
-const DateOverlay = styled.p`
-  position: absolute;
-  z-index: 1;
-  color: white;
-  font-size: 30px;
-  font-weight: bolder;
-  top: 75%;
-  left: 2%;
-`;
-
 type MeetingCardProps = {
-  cardLink: string;
   imgSrc: string;
   imgAlt: string;
-  imgOverlayText: string;
+  meetingDate: string;
   committee: string;
   tags: string[];
   excerpt?: string;
 };
 
 const MeetingCard = ({
-  cardLink,
   imgSrc,
   imgAlt,
-  imgOverlayText,
+  meetingDate,
   committee,
   tags,
   excerpt,
@@ -35,18 +22,27 @@ const MeetingCard = ({
 
   return (
     <section className="mzp-c-card mzp-has-aspect-16-9">
-      <a className="mzp-c-card-block-link" href={cardLink}>
+      <div>
         <div className="mzp-c-card-media-wrapper">
-          <DateOverlay>{imgOverlayText}</DateOverlay>
           <img className="mzp-c-card-image" src={imgSrc} alt={imgAlt} />
         </div>
         <div className="mzp-c-card-content">
           <div className="mzp-c-card-tag">Committee</div>
-          <h2 className="mzp-c-card-title">{committee}</h2>
-          {excerpt ? <p className="mzp-c-card-desc">{`"${excerpt}"`}</p> : null}
-          <p className="mzp-c-card-meta">Tags: {tagString}</p>
+          <h2 className="mzp-c-card-title">{meetingDate}</h2>
+          <p className="mzp-c-card-desc">{committee}</p>
+          {excerpt ? (
+            <p
+              className="mzp-c-card-desc"
+              style={{
+                fontStyle: "italic",
+                marginTop: "1rem",
+              }}
+            >{`"${excerpt}"`}</p>
+          ) : null}
+          <p className="mzp-c-card-meta">Tags</p>
+          <p className="mzp-c-card-desc">{tagString}</p>
         </div>
-      </a>
+      </div>
     </section>
   );
 };
