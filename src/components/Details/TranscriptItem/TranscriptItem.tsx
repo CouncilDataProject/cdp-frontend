8b1d7b82b638290505c327fef8a7c829963aa210
To Huynh <huynh.nto@gmail.com>
2021-05-26 19:39:11 -0700

    Use `user` icon instead of `user-circle`

--- src/components/Details/TranscriptItem/TranscriptItem.tsx
+++ src/components/Details/TranscriptItem/TranscriptItem.tsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import Highlighter from "react-highlight-words";
 import styled from "@emotion/styled";
 
+import DefaultAvatar from "../../Shared/DefaultAvatar";
+
 import { fontSizes } from "../../../styles/fonts";
 import { screenWidths } from "../../../styles/mediaBreakpoints";
 
@@ -56,22 +58,6 @@ const DefaultAvatarContainer = styled.div({
   width: AVATAR_SIZE,
   height: AVATAR_SIZE,
 });
-const DEFAULT_AVATAR = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    className="h-6 w-6"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth={2}
-      d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-    />
-  </svg>
-);
 
 const Actions = styled.div({
   display: "grid",
@@ -126,7 +112,9 @@ const TranscriptItem: FC<TranscriptItemProps> = ({
   const avatar = speakerPictureSrc ? (
     <SpeakerPicture src={speakerPictureSrc} alt={speakerName} />
   ) : (
-    <DefaultAvatarContainer>{DEFAULT_AVATAR}</DefaultAvatarContainer>
+    <DefaultAvatarContainer>
+      <DefaultAvatar />
+    </DefaultAvatarContainer>
   );
 
   return (
