53e40b8bd63e50036581d370c87d9befa2ae770d
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-09-10 11:17:08 -0700

    Add indexed event gram service for unpacking grams

---
+++ src/networking/IndexedEventGramService.ts
@@ -0,0 +1,33 @@
+import ModelService from "./ModelService";
+import { COLLECTION_NAME } from "./PopulationOptions";
+
+import IndexedEventGram from "../models/IndexedEventGram";
+import { where, limit, orderBy } from "firebase/firestore";
+import { WHERE_OPERATOR } from "./constants";
+
+export default class IndexedEventGramService extends ModelService {
+  constructor() {
+    super(COLLECTION_NAME.IndexedEventGram);
+  }
+
+  async getMatchingGrams(stemmedGram: string): Promise<IndexedEventGram[]> {
+    // We explictely do not populate the events here because it is likely that the
+    // same event will be requested multiple times, we will request event details after
+    // we have received all back and condensed to a set of events
+    const networkResponse = this.networkService.getDocuments(COLLECTION_NAME.IndexedEventGram, [
+      where("stemmed_gram", WHERE_OPERATOR.eq, stemmedGram),
+    ]);
+
+    return this.createModels(networkResponse, IndexedEventGram, `getMatchingGrams(${stemmedGram})`);
+  }
+
+  async getKeyGramsForEvent(eventRef: string): Promise<IndexedEventGram[]> {
+    const networkResponse = this.networkService.getDocuments(COLLECTION_NAME.IndexedEventGram, [
+      where("event_ref", WHERE_OPERATOR.eq, eventRef),
+      orderBy("value", "desc"),
+      limit(5),
+    ]);
+
+    return this.createModels(networkResponse, IndexedEventGram, `getKeyGramsForEvent(${eventRef})`);
+  }
+}
