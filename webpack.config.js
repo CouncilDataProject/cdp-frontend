ee1c185551126f61d00f2a7e9a1a81382e577ead
Jackson Maxfield Brown <jmaxfieldbrown@gmail.com>
2021-09-26 20:49:07 -0700

    admin/minor-fixes-and-export-specific-css-files (#97)
    * Pull required render deps to deps
    
    * Remove example topics asset in favor of in file
    
    * Fix filterpopup after upgrade
    
    * Export vjs theme css with webpack
    
    * Default to speaker index 0

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,13 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: "./src/index.ts",
+  entry: {
+    index: "./src/index.ts",
+    "vjs-theme-cdp": "./src/components/Details/EventVideo/vjs-theme-cdp.css",
+  },
   output: {
     publicPath: "",
-    filename: "index.js",
+    filename: "[name].js",
     library: "CDPFrontend", // TODO CHANGEME
     libraryTarget: "umd",
     path: path.resolve(__dirname, "dist"),
@@ -46,5 +49,5 @@ module.exports = {
     react: "react",
     "semantic-ui-react": "semantic-ui-react",
   },
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [new MiniCssExtractPlugin({ filename: "[name].css" })],
 };
