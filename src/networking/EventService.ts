ab87c791fb8f0a085a81aeac28a105aa642c14f1
To Huynh <huynh.nto@gmail.com>
2021-11-04 18:06:30 -0700

    Add getEvents, getRenderableEvent to eventService

--- src/networking/EventService.ts
+++ src/networking/EventService.ts
@@ -1,3 +1,18 @@
+import {
+  QueryConstraint,
+  where,
+  Timestamp,
+  doc,
+  orderBy,
+  limit,
+  startAfter,
+} from "@firebase/firestore";
+import { getStorage, ref, getDownloadURL } from "@firebase/storage";
+
+import { FirebaseConfig } from "../app/AppConfigContext";
+
+import { NetworkService } from "./NetworkService";
+import IndexedEventGramService from "./IndexedEventGramService";
 import ModelService from "./ModelService";
 import {
   COLLECTION_NAME,
@@ -5,13 +20,24 @@ import {
   PopulationOptions,
   REF_PROPERTY_NAME,
 } from "./PopulationOptions";
+import { ORDER_DIRECTION, WHERE_OPERATOR } from "./constants";
 
 import Event from "../models/Event";
-import { FirebaseConfig } from "../app/AppConfigContext";
+
+import { createError } from "../utils/createError";
+
+export interface RenderableEvent extends Event {
+  keyGrams: string[];
+  staticThumbnailURL: string;
+  hoverThumbnailURL: string;
+}
 
 export default class EventService extends ModelService {
+  indexedEventGramService: IndexedEventGramService;
+
   constructor(firebaseConfig: FirebaseConfig) {
     super(COLLECTION_NAME.Event, firebaseConfig);
+    this.indexedEventGramService = new IndexedEventGramService(firebaseConfig);
   }
 
   async getEventById(eventId: string): Promise<Event> {
@@ -35,5 +61,94 @@ export default class EventService extends ModelService {
     );
 
     return this.createModel(networkResponse, Event, `getFullEventById(${eventId})`);
+  }
+
+  async getEvents(
+    eventsPerPage: number,
+    bodyIds: string[],
+    dateRange: { start?: Date; end?: Date },
+    sort: { by: string; order: ORDER_DIRECTION },
+    startAfterEventDate?: Date
+  ): Promise<Event[]> {
+    if (bodyIds.length > 10) {
+      // allow only <= 10 bodies filtering
+      return Promise.reject(new Error("Number of bodies exceeded 10."));
+    }
+
+    const queryConstraints: QueryConstraint[] = [];
+
+    if (bodyIds.length > 0) {
+      const bodyDocRefs = bodyIds.map((bodyId) =>
+        doc(NetworkService.getDb(), COLLECTION_NAME.Body, bodyId)
+      );
+      queryConstraints.push(where(REF_PROPERTY_NAME.EventBodyRef, WHERE_OPERATOR.in, bodyDocRefs));
+    }
+
+    if (dateRange.start) {
+      const start = Timestamp.fromDate(dateRange.start);
+      queryConstraints.push(where("event_datetime", WHERE_OPERATOR.gteq, start));
+    }
+    if (dateRange.end) {
+      const end = Timestamp.fromDate(dateRange.end);
+      queryConstraints.push(where("event_datetime", WHERE_OPERATOR.lteq, end));
+    }
+
+    queryConstraints.push(orderBy(sort.by, sort.order));
+
+    if (startAfterEventDate) {
+      const startAfterDate = Timestamp.fromDate(startAfterEventDate);
+      queryConstraints.push(startAfter(startAfterDate));
+    }
+
+    queryConstraints.push(limit(eventsPerPage));
+
+    const networkResponse = this.networkService.getDocuments(
+      COLLECTION_NAME.Event,
+      queryConstraints,
+      new PopulationOptions([
+        new Populate(COLLECTION_NAME.Body, REF_PROPERTY_NAME.EventBodyRef),
+        new Populate(COLLECTION_NAME.File, REF_PROPERTY_NAME.EventStaticThumbnailRef),
+        new Populate(COLLECTION_NAME.File, REF_PROPERTY_NAME.EventHoverThumbnailRef),
+      ])
+    );
+
+    return this.createModels(
+      networkResponse,
+      Event,
+      `getEvents([${bodyIds}], ${JSON.stringify(dateRange)}, ${JSON.stringify(sort)}, ${
+        startAfterEventDate?.toISOString
+      }})`
+    );
+  }
+
+  async getRenderableEvent(event: Event): Promise<RenderableEvent> {
+    try {
+      const eventKeyGrams = await this.indexedEventGramService.getKeyGramsForEvent(
+        `${COLLECTION_NAME.Event}/${event.id}`
+      );
+      // Unpack keygrams to get strings
+      const keyGrams = eventKeyGrams.reduce((list, gram) => {
+        if (gram.unstemmed_gram !== undefined) {
+          list.push(gram.unstemmed_gram);
+        }
+        return list;
+      }, [] as string[]);
+      // Get https storage URLs
+      const storage = getStorage();
+      const staticThumbnailPathRef = ref(storage, event.static_thumbnail?.uri);
+      const hoverThumbnailPathRef = ref(storage, event.hover_thumbnail?.uri);
+      const staticThumbnailURL = await getDownloadURL(staticThumbnailPathRef);
+      const hoverThumbnailURL = await getDownloadURL(hoverThumbnailPathRef);
+      return Promise.resolve({
+        ...event,
+        keyGrams,
+        staticThumbnailURL,
+        hoverThumbnailURL,
+      });
+    } catch (err) {
+      const error = createError(err);
+      error.message = `eventService_getRenderableEvent(${JSON.stringify(event)})_${error.message}`;
+      return Promise.reject(error);
+    }
   }
 }
