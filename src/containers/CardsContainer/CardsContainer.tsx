5ab47864d6c353d83363ada228c8af5b7cb33216
To Huynh <huynh.nto@gmail.com>
2021-11-23 20:52:04 -0800

    Add link to card container

--- src/containers/CardsContainer/CardsContainer.tsx
+++ src/containers/CardsContainer/CardsContainer.tsx
@@ -1,6 +1,11 @@
-import React, { FC, ReactNode } from "react";
+import React, { FC } from "react";
+import { Link } from "react-router-dom";
 import styled from "@emotion/styled";
 
+import { Card } from "./types";
+
+import colors from "../../styles/colors";
+import { fontSizes } from "../../styles/fonts";
 import { screenWidths } from "../../styles/mediaBreakpoints";
 
 const Container = styled.div({
@@ -20,11 +25,30 @@ const Container = styled.div({
 });
 
 export interface CardsContainerProps {
-  cards: ReactNode;
+  cards: Card[];
 }
 
 const CardsContainer: FC<CardsContainerProps> = ({ cards }: CardsContainerProps) => {
-  return <Container>{cards}</Container>;
+  return (
+    <Container>
+      {cards.map(({ link, jsx }) => {
+        return (
+          <div key={link}>
+            <Link
+              to={link}
+              style={{
+                textDecoration: "none",
+                color: colors.black,
+                fontSize: fontSizes.font_size_6,
+              }}
+            >
+              {jsx}
+            </Link>
+          </div>
+        );
+      })}
+    </Container>
+  );
 };
 
 export default CardsContainer;
