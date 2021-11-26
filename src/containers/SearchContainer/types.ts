b064ca612e595d77cd891f95f3c6a6bc86933641
To Huynh <huynh.nto@gmail.com>
2021-11-25 20:14:16 -0800

    Keep only the search state prop

--- src/containers/SearchContainer/types.ts
+++ src/containers/SearchContainer/types.ts
@@ -1,12 +1,5 @@
-import { RenderableEvent } from "../../networking/EventSearchService";
-
 import { SearchState } from "../../pages/SearchPage/types";
 
 export interface SearchContainerData {
   searchState: SearchState;
-  eventResult: {
-    events: RenderableEvent[];
-    total: number;
-  };
-  //TODO: add legislation result
 }
