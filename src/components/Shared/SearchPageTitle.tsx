894ddbbe51313fc63e8b8967cdb7c3983548ad87
To Huynh <huynh.nto@gmail.com>
2021-11-28 16:03:20 -0800

    Combine the margin bottom

--- src/components/Shared/SearchPageTitle.tsx
+++ src/components/Shared/SearchPageTitle.tsx
@@ -7,15 +7,11 @@ const SearchPageTitle = styled.div({
   display: "flex",
   flexDirection: "column",
   rowGap: 32,
-  "& > h1": {
-    marginBottom: 0,
-  },
-  "& form": {
+  "& > h1, & form, & input": {
     marginBottom: 0,
   },
   "& input": {
     width: "100%",
-    marginBottom: 0,
   },
   [`@media (min-width:${screenWidths.tablet})`]: {
     flexDirection: "row",
