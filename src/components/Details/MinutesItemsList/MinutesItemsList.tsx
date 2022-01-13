80295c2748ea6987c6be55a95a27b912cb8c6d40
To Huynh <37560480+tohuynh@users.noreply.github.com>
2022-01-12 16:41:56 -0800

    feature/link-event-minutes-item-to-matter-page (#170)
    * Link event minutes item that have a matter ref to the matter page
    
    * Change link to `Go to Full Legislation Details`

--- src/components/Details/MinutesItemsList/MinutesItemsList.tsx
+++ src/components/Details/MinutesItemsList/MinutesItemsList.tsx
@@ -1,7 +1,9 @@
 import React, { FC } from "react";
 import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
 
 import DocumentsList from "./DocumentsList";
+import ChevronDownIcon from "../../Shared/ChevronDownIcon";
 
 import { Item } from "./types";
 
@@ -9,6 +11,15 @@ const ListItem = styled.li({
   "& > div:first-of-type": {
     // bold the minutes item's name
     fontWeight: 600,
+  },
+  "& > a": {
+    display: "flex",
+    alignItems: "center",
+  },
+  "& > a > svg": {
+    width: "1rem",
+    height: "1rem",
+    rotate: "-90deg",
   },
 });
 
@@ -26,6 +37,11 @@ const MinutesItemsList: FC<MinutesItemsListProps> = ({ minutesItems }: MinutesIt
         return (
           <ListItem key={elem.name}>
             <div>{elem.name}</div>
+            {elem.matter_ref && (
+              <Link to={`/matters/${elem.matter_ref}`}>
+                {"Go to Full Legislation Details"} <ChevronDownIcon />
+              </Link>
+            )}
             {elem.description && <div>{elem.description}</div>}
             <DocumentsList documents={elem.documents} />
           </ListItem>
