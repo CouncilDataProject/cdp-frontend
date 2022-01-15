e6dc8e97975dfc542718961afb6e56e85cc32d53
Brian Ledbetter <Brian.Ledbetter@gettyimages.com>
2022-01-14 16:15:14 -0800

    Improved bios

--- src/containers/PersonContainer/Biography.tsx
+++ src/containers/PersonContainer/Biography.tsx
@@ -23,7 +23,7 @@ const Biography: FC<BiographyProps> = ({
   mattersSponsored,
 }: BiographyProps) => {
   const { municipality } = useAppConfigContext();
-  const { bioText, introText } = writeBiography(
+  const { bioText, memberships } = writeBiography(
     person,
     councilMemberRoles,
     roles,
@@ -34,8 +34,17 @@ const Biography: FC<BiographyProps> = ({
 
   return (
     <div style={{ display: "flex", flexDirection: "column", paddingTop: 120, paddingLeft: 100 }}>
-      <p className="mzp-c-card-desc">{introText}</p>
       <p className="mzp-c-card-desc">{bioText}</p>
+      {memberships && memberships.length > 0 && (
+        <div>
+          <p className="mzp-c-card-desc">They are currently part of the following bodies:</p>
+          <ul className="mzp-u-list-styled">
+            {memberships.map((membershipSentence: string, index: number) => {
+              return <li key={`${index}-membership-in-body`}>{membershipSentence}</li>;
+            })}
+          </ul>
+        </div>
+      )}
       {person.website && (
         <a target="_blank" rel="noopener noreferrer" href={person.website}>
           {linkText}
