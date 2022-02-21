ba6a7dc989964519db3a6ba14adca08f3708e150
To Huynh <37560480+tohuynh@users.noreply.github.com>
2022-02-21 15:48:10 -0800

    Update legislation card (#201)

--- src/components/Cards/LegislationCard/LegislationCard.stories.tsx
+++ src/components/Cards/LegislationCard/LegislationCard.stories.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react";
 
-import LegislationCard, { LegislationCardProps } from "./LegislationCard";
 import { MATTER_STATUS_DECISION } from "../../../models/constants";
+
+import LegislationCard, { LegislationCardProps } from "./LegislationCard";
+
+import { basicPerson } from "../../../stories/model-mocks/person";
 
 export default {
   component: LegislationCard,
@@ -11,26 +14,41 @@ export default {
 
 const Template: Story<LegislationCardProps> = (args) => <LegislationCard {...args} />;
 
-export const adopted = Template.bind({});
-adopted.args = {
+const exampleLegislation = {
   name: "Council Budget Action SPD-4-A-1",
+  excerpt:
+    "Add $175,000 GF (ongoing) in 2020 to SPD to contract with an Indigenous led organization that can assist the City with its efforts to end the Missing and Murdered Indigenous Women and Girls Crisis, and impose a proviso",
   status: MATTER_STATUS_DECISION.ADOPTED,
   date: "January 22nd, 2020",
   tags: ["barbara bailey boulevard", "ADU", "single-family", "cottage"],
+  sponsors: Array.from({ length: 5 }).map((_, i) => {
+    return {
+      ...basicPerson,
+      id: `${basicPerson.id}-${i}`,
+      name: `${basicPerson.name} ${i}`,
+    };
+  }),
 };
+
+export const adopted = Template.bind({});
+adopted.args = exampleLegislation;
 
 export const rejected = Template.bind({});
 rejected.args = {
-  name: "Council Budget Action SPD-4-A-1",
+  ...exampleLegislation,
   status: MATTER_STATUS_DECISION.REJECTED,
-  date: "January 22nd, 2020",
-  tags: ["barbara bailey boulevard", "ADU", "single-family", "cottage"],
 };
 
 export const inProgress = Template.bind({});
 inProgress.args = {
-  name: "Council Budget Action SPD-4-A-1",
+  ...exampleLegislation,
   status: MATTER_STATUS_DECISION.IN_PROGRESS,
-  date: "January 22nd, 2020",
-  tags: ["barbara bailey boulevard", "ADU", "single-family", "cottage"],
+};
+
+export const withSearchQuery = Template.bind({});
+withSearchQuery.args = {
+  ...exampleLegislation,
+  excerpt:
+    "... contract with an Indigenous led organization that can assist the City with its efforts ...",
+  query: "indigenous",
 };
