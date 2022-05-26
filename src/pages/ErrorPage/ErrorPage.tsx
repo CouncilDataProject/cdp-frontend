ba2cbdf6fb42358d76d3614cd7af951a881a9ee1
NeelApte <neelapte@hotmail.com>
2022-05-26 11:04:09 -0700

    Updated based on lint.

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -11,7 +11,7 @@ const ErrorPage: FC<ErrorPageProps> = ({ error }: ErrorPageProps) => {
 
   return (
     <div>
-      <h1 className="mzp-u-title-sm">"Sorry, we can’t find that page."</h1>
+      <h1 className="mzp-u-title-sm">Sorry, we can’t find that page.</h1>
       <p>{errorText}</p>
       <button className="mzp-c-button mzp-t-secondary mzp-t-md" onClick={() => history.goBack()}>
         Back
